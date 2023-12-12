ca8738ac796d2abeeec216c554bf5c1b2ee8e34f
Abdelrahman Aldaker <aldaker@kth.se>
2023-12-12 21:20:56 +0100

    Initial Commit

---
+++ CarScanner/CarScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarScannerApp.swift
+//  CarScanner
+//
+//  Created by Abood Daker on 2023-12-12.
+//
+
+import SwiftUI
+
+@main
+struct CarScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarScanner
+//
+//  Created by Abood Daker on 2023-12-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
